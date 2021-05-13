e26b7b941e8b2ba97d5ca87c533797e4ad07d0a2
Sergey4334 <sergey.melnichuk92@gmail.com>
2021-05-13 12:03:46 +0300

    add new functions for progect

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+  $(".datepik").datepicker({
+    dateFormat: "dd MM",
+    minDate: new Date($("#hiddendelivdate").val()),
+    monthNames: [
+      "января",
+      "Февраля",
+      "марта",
+      "апреля",
+      "мая",
+      "июня",
+      "июля",
+      "августа",
+      "сентября",
+      "октября",
+      "ноября",
+      "декабря",
+    ],
+    dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+  });
+  $(function () {
+    (function quantityProducts() {
+      var $quantityArrowMinus = $(".quantity-arrow-minus");
+      var $quantityArrowPlus = $(".quantity-arrow-plus");
+      var $quantityNum = $(".quantity-num");
+
+      $quantityArrowMinus.click(quantityMinus);
+      $quantityArrowPlus.click(quantityPlus);
+
+      function quantityMinus() {
+        if ($quantityNum.val() > 1) {
+          $quantityNum.val(+$quantityNum.val() - 1);
+        }
+      }
+
+      function quantityPlus() {
+        $quantityNum.val(+$quantityNum.val() + 1);
+      }
+    })();
+  });
+});
