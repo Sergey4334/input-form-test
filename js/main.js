292b9f30a3ed72a728e25ee796172470ae025b07
Sergey4334 <sergey.melnichuk92@gmail.com>
2021-05-13 13:09:14 +0300

    change functions js for input number

--- js/main.js
+++ js/main.js
@@ -20,15 +20,51 @@ $(function () {
   });
   $(function () {
     (function quantityProducts() {
-      var $quantityArrowMinus = $(".quantity-arrow-minus");
-      var $quantityArrowPlus = $(".quantity-arrow-plus");
-      var $quantityNum = $(".quantity-num");
+      var $quantityArrowMinus = $(".quantity1");
+      var $quantityArrowPlus = $(".quantity2");
+      var $quantityNum = $(".quantity-input");
 
       $quantityArrowMinus.click(quantityMinus);
       $quantityArrowPlus.click(quantityPlus);
 
       function quantityMinus() {
         if ($quantityNum.val() > 1) {
+          $quantityNum.val(+$quantityNum.val() - 1);
+        }
+      }
+
+      function quantityPlus() {
+        $quantityNum.val(+$quantityNum.val() + 1);
+      }
+    })();
+    (function quantityProducts() {
+      var $quantityArrowMinus = $(".quantity-arrow");
+      var $quantityArrowPlus = $(".quantity-arrow2");
+      var $quantityNum = $(".quantity-input2");
+
+      $quantityArrowMinus.click(quantityMinus);
+      $quantityArrowPlus.click(quantityPlus);
+
+      function quantityMinus() {
+        if ($quantityNum.val() > 1) {
+          $quantityNum.val(+$quantityNum.val() - 1);
+        }
+      }
+
+      function quantityPlus() {
+        $quantityNum.val(+$quantityNum.val() + 1);
+      }
+    })();
+    (function quantityProducts() {
+      var $quantityArrowMinus = $(".quantity-arrow-chil");
+      var $quantityArrowPlus = $(".quantity-arrow-chil2");
+      var $quantityNum = $(".quantity-input3");
+
+      $quantityArrowMinus.click(quantityMinus);
+      $quantityArrowPlus.click(quantityPlus);
+
+      function quantityMinus() {
+        if ($quantityNum.val() > 0) {
           $quantityNum.val(+$quantityNum.val() - 1);
         }
       }
